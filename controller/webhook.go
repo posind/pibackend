1556453bfb08e9a383008fe063da623ef5d4729b
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-01 19:13:09 +0700

    tambah informasi compare dan file yang diubah

--- controller/webhook.go
+++ controller/webhook.go
@@ -50,7 +50,7 @@ func PostWebHookGithub(respw http.ResponseWriter, req *http.Request) {
 		var dokcommit model.PushReport
 		for i, komit := range pyl.Commits {
 			kommsg := strings.TrimSpace(komit.Message)
-			appd := strconv.Itoa(i+1) + ". " + kommsg + "\n_" + komit.Author.Name + "_\n"
+			appd := strconv.Itoa(i+1) + ". " + kommsg + "\n_" + strings.Join(komit.Modified[:], "\n") + "_\n"
 			dokcommit = model.PushReport{
 				ProjectName: prj.Name,
 				Project:     prj,
@@ -96,7 +96,7 @@ func PostWebHookGithub(respw http.ResponseWriter, req *http.Request) {
 			}
 			komsg += appd
 		}
-		msg = "Nama: " + dokcommit.User.Name + "\nUserGitHub: " + pyl.Sender.Login + "\nRepo: " + pyl.Repository.Name + "\n Branch: " + pyl.Ref + "\n" + pyl.Repository.URL + "\n" + komsg
+		msg = "Nama: " + dokcommit.User.Name + "\nUserGitHub: " + pyl.Sender.Login + "\nRepo: " + pyl.Repository.Name + "\nBranch: " + pyl.Ref + "\n" + pyl.Compare + "\n" + komsg
 		dt := &model.TextMessage{
 			To:       prj.Owner.PhoneNumber,
 			IsGroup:  false,
