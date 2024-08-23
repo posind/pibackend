80069c9edbedf55ef2966eae895856047f6e6a99
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-23 22:11:41 +0700

    tutupin sama x kecil

--- helper/phone/phone.go
+++ helper/phone/phone.go
@@ -4,5 +4,5 @@ import "regexp"
 
 func MaskPhoneNumber(phone string) string {
 	re := regexp.MustCompile(`(\d{6})(\d{3})(\d{3,})`)
-	return re.ReplaceAllString(phone, `$1XXX$3`)
+	return re.ReplaceAllString(phone, `$1xxx$3`)
 }
