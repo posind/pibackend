41f84a372835ae030456131f0090572fc0114ed3
Rolly Maulana  Awangga <rolly@awang.ga>
2024-08-26 09:41:03 +0700

    hapus session menu fix bug

--- mod/helpdesk/helpdesk.go
+++ mod/helpdesk/helpdesk.go
@@ -236,8 +236,8 @@ func EndHelpdesk(Profile itmodel.Profile, Pesan itmodel.IteungMessage, db *mongo
 	}
 	//hapus hub
 	atdb.DeleteOneDoc(db, "hub", bson.M{"userphone": helpdeskuser.UserPhone, "adminphone": helpdeskuser.AdminPhone})
-	//hapus menu
-	atdb.DeleteOneDoc(db, "menu", bson.M{"userphone": helpdeskuser.UserPhone, "adminphone": helpdeskuser.AdminPhone})
+	//hapus session menu
+	atdb.DeleteOneDoc(db, "menu", bson.M{"phonenumber": Pesan.Phone_number})
 	//prefill message admin dan user
 	msgstradmin := GetPrefillMessage("admintutuphelpdesk", db) //pesan untuk admin
 	if helpdeskuser.UserName != "" {
